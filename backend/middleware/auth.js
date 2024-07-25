0d5c402f7ae86c27d2b56e29a5853b940b29a618
NeemaMwende <gracepete12@gmail.com>
2024-07-25 19:25:03 +0300

    authmiddleware for the user logins

---
+++ backend/middleware/auth.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken"
+import User from "../models/User.js"
+
+
+const authMiddleware = async (req, res, next) => {
+    const token = req.header("Authorization")?.replace('Bearer ','');
+
+    if (!token) {
+        return res.status(401).json({message: "No token provided"})
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findOne(decoded.userId);
+
+        if (!user) {
+            return res.status(404).json({message: "User not found"});
+        }
+
+        req.user = user;
+        next();
+
+
+    } catch (error) {
+        res.status(401).json({message: "Invalid token provided", error})
+    }
+}
+
+export default authMiddleware;
