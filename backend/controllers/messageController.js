5955ef99bd7029735268bc736c8f987653e0883f
NeemaMwende <gracepete12@gmail.com>
2024-07-25 21:32:24 +0300

    create conversation and get conversations by user logic

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -14,11 +14,24 @@ export const createConversation = async (req, res) => {
             return res.status(404).json({message: "One or more users not found"});
         }
 
+        const participantIds = users.map(user=>user._id)
+        
+        const conversation = new Conversation(participantIds);
+        await conversation.save();
+        res.status(200).json({conversation});
 
-        const newConversation = new Conversation(req.body);
-        await newConversation.save();
-        res.status(200).json({newConversation});
 
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+export const getConversationByUser = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const conversations = await Conversation.find({participants:userId});
+
+        res.status(200).json(conversations);
 
     } catch (error) {
         res.status(500).json({ message: error.message });
