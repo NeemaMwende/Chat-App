39cba5275348469a20da8614bc849faf6a68b1cb
NeemaMwende <gracepete12@gmail.com>
2024-07-25 17:25:10 +0300

    updates

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,38 @@
 import express from 'express';
+import { Server } from 'socket.io';
+import dotenv from 'dotenv';
+import http from 'http';
+import socket from 'socket.io-client';
+import mongoose from 'mongoose';
+
+const PORT = process.env.PORT || 5000;
 
 
 const app = express();
+dotenv.config()
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+});
+
+// const socket = io();
+// socket.io("connect", () => {
+//     console.log("Connected to server");
+// });
+
+mongoose.connect(process.env.MONGO_URI, 
+     //{ useNewUrlParser: true, useUnifiedTopology: true }
+).then(()=> console.log("Connected to MongoDB"));
+// mongoose.connection.once('open', () => console.log('Connected to MongoDB'));
+
+
+
+server.listen(PORT, () => console.log(`listening on port ${PORT}`));
