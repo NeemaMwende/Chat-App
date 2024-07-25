4538273ced0a2b4dfe8cf68d4e1eccd224d972ab
NeemaMwende <gracepete12@gmail.com>
2024-07-25 18:57:44 +0300

    updates

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -3,9 +3,22 @@ import Conversations from "./Conversations";
 
 const messageSchema = new mongoose.Schema({
    conversation : {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Conversations'
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Conversation'
+   },
+   sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+   },
+   text: {
+        type: String,
+        required: true
+   },
+   createdAt: {
+        type: Date,
+        default: Date.now()
    }
+
 });
 
 export default mongoose.model("Message", messageSchema);
