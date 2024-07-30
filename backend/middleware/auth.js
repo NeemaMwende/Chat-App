a0a89ff403717e6f70306847aaabe02aabd4b6e7
NeemaMwende <gracepete12@gmail.com>
2024-07-30 18:04:04 +0300

    auth middleware updates

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,29 +1,28 @@
-import jwt from "jsonwebtoken"
-import User from "../models/User.js"
-
+// middleware/auth.js
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
 
 const authMiddleware = async (req, res, next) => {
-    const token = req.header("Authorization")?.replace('Bearer ','');
+    const token = req.header('Authorization')?.replace('Bearer ', '');
 
     if (!token) {
-        return res.status(401).json({message: "No token provided"})
+        return res.status(401).json({ message: 'No token provided' });
     }
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findOne(decoded.userId);
+        console.log(decoded);
+        const user = await User.findById(decoded.userId);
 
         if (!user) {
-            return res.status(404).json({message: "User not found"});
+            return res.status(404).json({ message: 'User not found' });
         }
 
         req.user = user;
         next();
-
-
     } catch (error) {
-        res.status(401).json({message: "Invalid token provided", error})
+        res.status(401).json({ message: 'Invalid token' });
     }
-}
+};
 
 export default authMiddleware;
